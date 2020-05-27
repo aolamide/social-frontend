26cda7cb4f5c614e8d2321d7c4ee82d19f397203
Olamide <aboyejiolamide15@gmail.com>
2020-05-27 21:00:46 +0100

    add link to sign in text on after sign up

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { signup } from '../auth';
+import { Link } from 'react-router-dom';
 
 class Signup extends Component {
   constructor() {
@@ -65,7 +66,7 @@ class Signup extends Component {
           { error }
         </div>
         <div className="alert alert-info" style={{display : open ? '' : 'none'}}>
-          New account is created successfully. Please Sign In.
+          New account is created successfully. Please <Link to='/signin'>Sign In</Link>.
         </div>
         {this.signupForm(name, email, password)}
       </div>
